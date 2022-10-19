bdf3ffb945c10d5fced90047f6c8f8258da3853c
cola639 <337507950@qq.com>
2022-10-19 14:19:21 +0800

    FIRST COMMIT

---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/dev-api", {
+      target: "http://localhost:3100",
+      secure: false,
+      changeOrigin: true,
+      pathRewrite: {
+        "^/dev-api": "/api",
+      },
+    })
+  );
+};
